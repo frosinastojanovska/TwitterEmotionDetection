6c9b018626f32859e50fd1650522f92704f58252
frosinastojanovska <stojanovska.frose@gmail.com>
2018-03-26 17:00:35 +0200

    Add code for sentiment classification dataset

--- deep_semantic_model.py
+++ deep_semantic_model.py
@@ -3,7 +3,39 @@ import numpy as np
 import pandas as pd
 import keras as k
 import keras.layers as kl
+from keras.callbacks import ModelCheckpoint, CSVLogger
 from preprocessing import fix_encoding, split_tweet_sentences, tokenize_tweets, get_word_embeddings
+
+
+def cnn_model(num_classes, input_shape):
+    """ Creates CNN model for classification of emotions with word2vec embeddings
+
+    :param num_classes: number of classes
+    :type num_classes: int
+    :param input_shape: shape of the input
+    :type input_shape: tuple
+    :return: cnn model
+    """
+    model = k.Sequential()
+
+    model.add(kl.Convolution1D(32, 3, activation='relu', input_shape=input_shape))
+    model.add(kl.MaxPooling1D())
+    model.add(kl.Convolution1D(64, 3, activation='relu'))
+    model.add(kl.GlobalMaxPooling1D())
+    model.add(kl.Dense(128))
+    model.add(kl.Dropout(0.2))
+    model.add(kl.Activation('relu'))
+
+    model.add(kl.Dense(num_classes))
+    model.add(kl.Activation('sigmoid'))
+
+    opt = k.optimizers.Adam(amsgrad=True)
+    model.compile(optimizer=opt,
+                  loss='categorical_crossentropy',
+                  metrics=[k.metrics.categorical_accuracy,
+                           k.metrics.mae,
+                           k.metrics.top_k_categorical_accuracy])
+    return model
 
 
 def load_data():
@@ -12,7 +44,7 @@ def load_data():
     :return: list of word embeddings for each tweet, list of classes, number of classes
     :rtype: (list(numpy.array), list(int), int)
     """
-    df = pd.read_csv('data/text_emotion.csv')[:10]
+    df = pd.read_csv('data/text_emotion.csv')
 
     if os.path.exists('data/text_emotion_w2vec.npy'):
         word_embed = np.load('data/text_emotion_w2vec.npy')
@@ -37,38 +69,6 @@ def load_data():
     return word_embed, classes, len(c)
 
 
-def cnn_model(num_classes, input_shape):
-    """ Creates CNN model for classification of emotions with word2vec embeddings
-
-    :param num_classes: number of classes
-    :type num_classes: int
-    :param input_shape: shape of the input
-    :type input_shape: tuple
-    :return: cnn model
-    """
-    model = k.Sequential()
-
-    model.add(kl.Convolution1D(32, 3, activation='relu', input_shape=input_shape))
-    model.add(kl.Convolution1D(32, 3, activation='relu'))
-    model.add(kl.MaxPooling1D())
-    model.add(kl.Convolution1D(64, 3, activation='relu'))
-    model.add(kl.Convolution1D(64, 3, activation='relu'))
-    model.add(kl.MaxPooling1D())
-    model.add(kl.Flatten())
-    model.add(kl.Dropout(0.2))
-    model.add(kl.Dense(128, activation='relu'))
-    model.add(kl.Dropout(0.4))
-    model.add(kl.Dense(num_classes, activation='softmax'))
-
-    opt = k.optimizers.Adam(lr=0.01, amsgrad=True)
-    model.compile(optimizer=opt,
-                  loss='categorical_crossentropy',
-                  metrics=[k.metrics.categorical_accuracy,
-                           k.metrics.mae,
-                           k.metrics.top_k_categorical_accuracy])
-    return model
-
-
 if __name__ == '__main__':
     data_X, data_y, n_classes = load_data()
     split = 30000
@@ -77,11 +77,17 @@ if __name__ == '__main__':
     test_X = data_X[split:]
     test_y = data_y[split:]
     shape = train_X[0].shape
+    train_y = k.utils.to_categorical(train_y, n_classes)
+    test_y = k.utils.to_categorical(test_y, n_classes)
 
     model = cnn_model(n_classes, shape)
-    train_y = k.utils.to_categorical(train_y, n_classes)
-    model.fit(train_X, train_y, nb_epoch=200)
-    model.save_weights("models/cnn_semantic_model.h5")
-    test_y = k.utils.to_categorical(test_y, n_classes)
+    # checkpoint
+    filepath = "models/cnn_semantic_model-{epoch:02d}-{val_loss:.2f}.h5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True,
+                                 save_weights_only=True, mode='min')
+    csv_logger = CSVLogger('logs/cnn_semantic_model.log')
+    model.fit(train_X, train_y, epochs=200, callbacks=[checkpoint, csv_logger], validation_split=0.2)
+    # model.save_weights("models/cnn_semantic_model.h5")
+
     score = model.evaluate(test_X, test_y, batch_size=128)
     np.savetxt('scores/cnn_semantic_model.txt', np.array(score))
---
+++ sentiment_analysis_deep_learning.py
@@ -0,0 +1,58 @@
+import os
+import keras as k
+import numpy as np
+import pandas as pd
+from deep_semantic_model import cnn_model
+from preprocessing import fix_encoding, split_tweet_sentences, tokenize_tweets, get_word_embeddings
+
+
+def load_data():
+    df = pd.read_csv('data/data_sentiment.csv', header=None, names=['sentiment', 'tweet_id',
+                                                                    'date', 'query', 'user',
+                                                                    'tweet'], encoding='latin-1')
+
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    if os.path.exists('data/text_sentiment_w2vec.npy'):
+        word_embed = np.load('data/text_sentiment_w2vec.npy')
+    else:
+        df = fix_encoding(df)
+        df = split_tweet_sentences(df)
+        df = tokenize_tweets(df)
+        df = get_word_embeddings(df, 100)
+        word_embed = df['embeddings'].values
+        word_embed = np.stack(word_embed, axis=0)
+        np.save('data/text_sentiment_w2vec', word_embed)
+
+    df[df.sentiment == 4] = 1
+    classes = df['sentiment'].values.tolist()
+    c = np.unique(classes).tolist()
+
+    return word_embed, classes, len(c)
+
+
+def cnn_sentiment_classification():
+    data_X, data_y, n_classes = load_data()
+    split = 1280000
+    train_X = data_X[:split]
+    train_y = data_y[:split]
+    test_X = data_X[split:]
+    test_y = data_y[split:]
+    shape = train_X[0].shape
+    train_y = k.utils.to_categorical(train_y, n_classes)
+    test_y = k.utils.to_categorical(test_y, n_classes)
+    model = cnn_model(n_classes, shape)
+    # checkpoint
+    filepath = "models/cnn_semantic_model-{epoch:02d}-{val_loss:.2f}.h5"
+    checkpoint = k.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True,
+                                             save_weights_only=True, mode='min')
+    csv_logger = k.callbacks.CSVLogger('logs/cnn_semantic_model.log')
+    model.fit(train_X, train_y, epochs=200, callbacks=[checkpoint, csv_logger], validation_split=0.2)
+    # model.save_weights("models/cnn_semantic_model.h5")
+
+    score = model.evaluate(test_X, test_y, batch_size=128)
+    np.savetxt('scores/cnn_semantic_model.txt', np.array(score))
+
+
+if __name__ == '__main__':
+    cnn_sentiment_classification()
