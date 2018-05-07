93bf2faf23b36bc5dbf84b539261c60ce9ebd244
frosinastojanovska <stojanovska.frose@gmail.com>
2018-05-08 00:46:20 +0200

    Emotion word embeddings cosine similarity

--- embeddings_vectors_tests.py
+++ embeddings_vectors_tests.py
@@ -1,5 +1,6 @@
 import os
 from gensim.models import KeyedVectors
+from scipy.spatial import distance
 
 
 def original_glove_embeddings():
@@ -10,13 +11,12 @@ def original_glove_embeddings():
         glove2word2vec(glove_file, word2vec_file)
     model = KeyedVectors.load_word2vec_format(word2vec_file, binary=False)
     # model.save_word2vec_format(word2vec_file + '.bin', binary=True)
-    # print(model.most_similar(positive=['happy'], negative=[], topn=10))
-    # print(model.most_similar(positive=['sad'], negative=[], topn=10))
-    # print(model.most_similar(positive=['angry'], negative=[], topn=10))
-    # print(model.most_similar(positive=['joy'], negative=[], topn=10))
-    print(model.most_similar(positive=['joy', 'trust'], negative=[], topn=10))
-    print(model.most_similar(positive=['joy', 'fear'], negative=[], topn=10))
-    print(model.most_similar(positive=['anger', 'joy'], negative=[], topn=10))
+    sim = emotion_arithmetic_test_closeness(model)
+    print(sim)
+    print(sum(sim)/len(sim))
+    sim = emotion_arithmetic_test_opposite(model)
+    print(sim)
+    print(sum(sim) / len(sim))
 
 
 def original_emoji2vec_embeddings():
@@ -33,13 +33,45 @@ def edim_word2vec_embeddings():
         glove2word2vec(original_file, word2vec_file)
     model = KeyedVectors.load_word2vec_format(word2vec_file, binary=False)
     # model.save_word2vec_format(word2vec_file + '.bin', binary=True)
-    # print(model.most_similar(positive=['happy'], negative=[], topn=10))
-    # print(model.most_similar(positive=['sad'], negative=[], topn=10))
-    # print(model.most_similar(positive=['angry'], negative=[], topn=10))
-    # print(model.most_similar(positive=['joy'], negative=[], topn=10))
-    print(model.most_similar(positive=['joy', 'trust'], negative=[], topn=10))
-    print(model.most_similar(positive=['joy', 'fear'], negative=[], topn=10))
-    print(model.most_similar(positive=['anger', 'joy'], negative=[], topn=10))
+    sim = emotion_arithmetic_test_closeness(model)
+    print(sim)
+    print(sum(sim) / len(sim))
+    sim = emotion_arithmetic_test_opposite(model)
+    print(sim)
+    print(sum(sim) / len(sim))
+
+
+def emotion_arithmetic_test_closeness(model):
+    vectors = [('joy', 'trust', 'love'), ('joy', 'fear', 'guilt'), ('joy', 'surprise', 'delight'),
+               ('trust', 'fear', 'submission'), ('trust', 'surprise', 'curiosity'),
+               ('trust', 'sadness', 'sentimentality'),
+               ('fear', 'surprise', 'awe'), ('fear', 'sadness', 'despair'), ('fear', 'disgust', 'shame'),
+               ('surprise', 'sadness', 'disappointment'), ('surprise', 'disgust', 'unbelief'),
+               ('surprise', 'anger', 'outage'),
+               ('sadness', 'disgust', 'remorse'), ('sadness', 'anger', 'envy'), ('sadness', 'anticipation', 'pessimism'),
+               ('disgust', 'anger', 'contempt'), ('disgust', 'anticipation', 'cynicism'),
+               # ('disgust', 'joy', 'morbidness'),
+               ('anger', 'anticipation', 'aggression'), ('anger', 'joy', 'pride'), ('anger', 'trust', 'dominance'),
+               ('anticipation', 'joy', 'optimism'), ('anticipation', 'trust', 'hope'),
+               ('anticipation', 'fear', 'anxiety')
+               ]
+    sim = []
+    for x, y, z in vectors:
+        _sim = 1 - distance.cosine(model[x] + model[y], model[z])
+        sim.append(_sim)
+
+    return sim
+
+
+def emotion_arithmetic_test_opposite(model):
+    vectors = [('anticipation', 'surprise'), ('anger', 'fear'),
+               ('disgust', 'trust'), ('sadness', 'joy')]
+    sim = []
+    for x, y in vectors:
+        _sim = 1 - distance.cosine(model[x], model[y])
+        sim.append(_sim)
+
+    return sim
 
 
 if __name__ == '__main__':
